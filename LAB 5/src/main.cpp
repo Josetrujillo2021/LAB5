c8ca488a383c0d099f49397a7ef0453fc9955812
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-09-29 15:40:37 -0600

    Creación de funciones PWM Y MAP
    Se utilizaron la función de MAP Para poder hacer una equivalencia de un valor de 0 a 1023 de resolución del ADC a una resolución de 8 bits

--- LAB 5/src/main.cpp
+++ LAB 5/src/main.cpp
@@ -20,36 +20,33 @@
 #define RS 27
 #define E 14
 
-
+#define freq 50
 #define resolucion 8
 #define LRChannel 1
 #define LVChannel 2
 #define LAChannel 3
 
+#define LR 4
+#define LV 2
+#define LA 15
+
 //----------------------------------------------------------------------------------------------------------------------
 //Prototipos de funciones
 //----------------------------------------------------------------------------------------------------------------------
-
+void LectorVoltajes(void);
+void PWMLEDS(V1, V2); 
+void configurarPWM(void); 
 //---------------------------------------------------------------------------------------------------------------------
 //Variables Globales
 //----------------------------------------------------------------------------------------------------------------------
 
 //Creación de las variables que reciben el dato analogico del voltaje
-float V1 = 0.0; 
-float V2 = 0.0; 
+int V1 = 0; 
+int V2 = 0; 
 
 //Creación de variables de comvierten el dato de Analógico a digital
 int LR = 0; 
 int LV = 0; 
-
-//Generación para las variables de cada uno de los datos mostrados en la LCD 
-int decenas1 = 0; 
-int unidades1 = 0; 
-int decimales1 = 0;
-
-int decenas2 = 0; 
-int unidades2 = 0; 
-int decimales2 = 0;
 
 int contador = 0; 
 LiquidCrystal LCD(RS, E, D4, D5, D6, D7);
@@ -68,7 +65,22 @@ int DutyCicleLA = 0; //dutycicle del led AZUL
 //CONFIGURACIÓN
 //----------------------------------------------------------------------------------------------------------------------
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
+
+  configurarPWM();
+  // Este comando limpia la LCD
+  LCD.begin(16, 2);
+
+  pinMode(LR, OUTPUT);
+  pinMode(LV, OUTPUT); 
+  pinMode(LA, OUTPUT);
+
+  digitalWrite(LR, LOW);
+  digitalWrite(LV, LOW);
+  digitalWrite(LA, LOW);
+  
+  analogResolution(resolucion);
+   
 }
 
 
@@ -76,5 +88,42 @@ void setup() {
 //Loop principal
 //---------------------------------------------------------------------------------------------------------------------
 void loop() {
-  // put your main code here, to run repeatedly:
+  LectorVoltajes();
+  PWMLEDS(V1, V2);
+  
+  }
+//---------------------------------------------------------------------------------------------------------------------
+//Lector analogico de voltajes
+//---------------------------------------------------------------------------------------------------------------------
+void LectorVoltajes(void){
+  V1 = analogReadMillivolts(Voltaje1);
+  V2 = analogReadMilliVolts(Voltaje2);
+  
+  V1 = map(V1, 0,1023, 0, 255);
+  V2 = map(V2, 0, 1023, 0, 255); 
+  }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//FUNCIÓN PARA CONFIGURAR PWM General
+//----------------------------------------------------------------------------------------------------------------------
+
+void configurarPWM(void){
+  //Configurando PWM para los leds
+
+  ledcSetup (LRChannel, freq, resolucion); 
+  ledcAttachPin(LR, LRChannel);
+
+  ledcSetup (LVChannel, freq, resolucion); 
+  ledcAttachPin(LV, LVChannel);
+
+  ledcSetup (LAChannel, freq, resolucion); 
+  ledcAttachPin(LA, LAChannel);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//PWM LEDS
+//---------------------------------------------------------------------------------------------------------------------
+void PWMLEDS(int x, int y){
+
 }
