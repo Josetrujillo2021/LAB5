db3a041949d244aa31b165aa2709615259573f38
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-09-29 15:17:01 -0600

    Definicion pines y variables
    Se generaron los pines a utilizar en el ESP32 y las variables

--- LAB 5/src/main.cpp
+++ LAB 5/src/main.cpp
@@ -9,6 +9,22 @@
 //----------------------------------------------------------------------------------------------------------------------
 //Definición de pines
 //----------------------------------------------------------------------------------------------------------------------
+#define Voltaje1 13
+#define Voltaje2 12
+
+#define D4 32
+#define D5 33
+#define D6 25
+#define D7 26
+
+#define RS 27
+#define E 14
+
+
+#define resolucion 8
+#define LRChannel 1
+#define LVChannel 2
+#define LAChannel 3
 
 //----------------------------------------------------------------------------------------------------------------------
 //Prototipos de funciones
@@ -18,6 +34,32 @@
 //Variables Globales
 //----------------------------------------------------------------------------------------------------------------------
 
+//Creación de las variables que reciben el dato analogico del voltaje
+float V1 = 0.0; 
+float V2 = 0.0; 
+
+//Creación de variables de comvierten el dato de Analógico a digital
+int LR = 0; 
+int LV = 0; 
+
+//Generación para las variables de cada uno de los datos mostrados en la LCD 
+int decenas1 = 0; 
+int unidades1 = 0; 
+int decimales1 = 0;
+
+int decenas2 = 0; 
+int unidades2 = 0; 
+int decimales2 = 0;
+
+int contador = 0; 
+LiquidCrystal LCD(RS, E, D4, D5, D6, D7);
+
+
+//Dutycicle de cada led 
+int DutyCicleLR = 0; //dutycicle del led ROJO
+int DutyCicleLV = 0; //dutycicle del led VERDE
+int DutyCicleLA = 0; //dutycicle del led AZUL
+
 //----------------------------------------------------------------------------------------------------------------------
 //ISR  (interrupciones)
 //----------------------------------------------------------------------------------------------------------------------
@@ -26,7 +68,7 @@
 //CONFIGURACIÓN
 //----------------------------------------------------------------------------------------------------------------------
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); 
 }
 
 
